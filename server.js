2e76d571b58e4677cea73a9a645c525d993d3979
BokeeJ <bokeejusthard@gmail.com>
2025-05-21 21:35:02 +0200

    finished

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 4000
 
 // === 1. POVEZIVANJE SA MONGODB ATLAS ===
 
-const uri = "mongodb+srv://bokeejusthard:zeka@cluster0.ejxukug.mongodb.net/kokko?retryWrites=true&w=majority&appName=Cluster0";
+const uri = process.env.MONGO_URI;
 
 const client = new MongoClient(uri)
 let collection
